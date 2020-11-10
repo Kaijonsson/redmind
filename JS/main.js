5fe5089d04a65aac8c55715a47a7423c4e177792
Axel Jönsson <axel.jonsson@hotmail.com>
2020-11-10 13:56:59 +0100

    music done

--- JS/main.js
+++ JS/main.js
@@ -17,7 +17,7 @@ async function getStarWars() {
     const characterResponse = await fetch('https://swapi.dev/api/people');
     const characterData = await characterResponse.json();
 //--------------------------------------------------------------------//
-
+    let starWarsClass = new StarWarsClass;
     console.log(characterData.results[0]);
 
     let counter = 0;
@@ -42,80 +42,13 @@ async function getStarWars() {
             let cardHandler = document.getElementById("characterCard");
 
             if(cardHandler == null) {
-                createStarWarsCharacterCard();
+                starWarsClass.createStarWarsCharacterCard(swCharacter);
 
             } else {
                 cardHandler.parentNode.childNodes[3].remove();
-                createStarWarsCharacterCard()
-            }
-
-            
+                starWarsClass.createStarWarsCharacterCard(swCharacter)
+            }            
         })
-
-        function createStarWarsCharacterCard() {
-            //changing layout of application on "click".
-            setClassGrid();
-
-            let btn = document.createElement("button");
-            btn.setAttribute("id", "exitButton")
-            btn.innerText = "X";
-
-            btn.addEventListener("click", function (event) {
-                event.preventDefault();
-                btn.parentNode.remove();
-                setClassFlex();
-            })
-            //creates new div to present clicked character with more info.
-            let newDiv = document.createElement("div");
-            newDiv.setAttribute("id", "characterCard");
-            let ref = document.getElementById("mainListId");
-            let parent = document.getElementById("mainListId").parentNode;
-            parent.insertBefore(newDiv, ref);
-            newDiv.appendChild(btn);
-            newDiv.appendChild(getStarWarsCharacterInfo(swCharacter))
-        return newDiv;
-    }
-
-    function setClassGrid() {
-        document.getElementById("contentBoxId").setAttribute("class", "contentBox-grid");
-        document.getElementById("swTitle").setAttribute("class", "title-grid");
-        document.getElementById("mainListId").setAttribute("class", "mainList-grid");
-        document.getElementById("swFooter").setAttribute("class", "grid");
-
-    }
-    function setClassFlex() {
-        document.getElementById("contentBoxId").setAttribute("class", "contentBox-flex");
-        document.getElementById("swTitle").setAttribute("class", "title-flex");
-        document.getElementById("mainListId").setAttribute("class", "mainList-flex");
-        document.getElementById("swFooter").setAttribute("class", "grid");
-
-    }
-
-
-    }
-    function getStarWarsCharacterInfo(character){
-        let properties = [
-            "Name: "        + character.name, 
-            "Height: "      + character.height + "cm", 
-            "Gender: "      + character.gender,
-            "Birth Year: "  + character.birth_year,
-            "Mass: "        + character.mass + "kg",
-            "skin-color: "  + character.skin_color
-        ]
-
-        let characterUl = document.createElement("ul");
-        characterUl.setAttribute("id", "characterCardUl");
-
-        for (property of properties){
-            let characterList = document.createElement("li");
-            characterList.setAttribute("class", "characterCardClass");
-            characterList.innerHTML = property;
-
-            characterUl.appendChild(characterList);
-        }
-
-
-        return characterUl
     }
 
 
@@ -139,13 +72,3 @@ getStarWars()
 
 
 
-
-
-
-
-            // getCharacterStarship(swCharacter.starships[0]);
-
-            // async function getCharacterStarship(characterName){
-            //     const starShipResponse = await fetch(characterName);
-            //     const starshipData = await starShipResponse.json();
-            // }
